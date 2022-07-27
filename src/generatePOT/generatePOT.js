40c2e1c19906b84b459153a9cf2c9db917cd6bdb
Martin Nicholson <martin@taotesting.com>
2022-07-27 23:57:34 +0200

    fix: deduplicate based on msgid while generating POT

--- src/generatePOT/generatePOT.js
+++ src/generatePOT/generatePOT.js
@@ -13,19 +13,22 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA
+ * Copyright (c) 2020-2022 (original work) Open Assessment Technologies SA
  *
  */
 
 /**
  * Writes the strings to POT file
  *
- * Skips the duplicate strings
+ * Removes the duplicate strings
  * @param {Map<Object, Object[]}
  * @returns {string}
  */
 module.exports = function generatePOT(strings) {
     let potContent = '';
+
+    const msgids = new Set(); // avoids duplicates
+    const msgid_plurals = new Set();
 
     strings.forEach((contexts, meta) => {
         const messageContext = contexts.map(context => `#: ${context.file}:${context.line}`).join('\n');
@@ -34,7 +37,16 @@ module.exports = function generatePOT(strings) {
             return accumulator;
         }, '');
 
-        potContent += `${messageContext}\n${metaString}msgstr${meta.msgid_plural ? '[0]' : ''} ""\n`;
+        // add entry if msgid or msgid_plural are new
+        if ((meta.msgid && !msgids.has(meta.msgid)) || (meta.msgid_plural && !msgid_plurals.has(meta.msgid_plural))) {
+            potContent += `${messageContext}\n${metaString}msgstr${meta.msgid_plural ? '[0]' : ''} ""\n`;
+            if (meta.msgid) {
+                msgids.add(meta.msgid);
+            }
+            if (meta.msgid_plural) {
+                msgid_plurals.add(meta.msgid_plural);
+            }
+        }
     });
 
     return potContent;
