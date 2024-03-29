03753368656e1ff0260e4fa364cfc0603cb833e9
Martin Nicholson <martin@taotesting.com>
2024-03-29 12:24:28 +0100

    fix: proper ordering of POT file entries by first context string

--- src/rollup/i18n.js
+++ src/rollup/i18n.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2020-2024 (original work) Open Assessment Technologies SA ;
  */
 
 const path = require('path');
@@ -22,6 +22,31 @@ const { minimatch } = require('minimatch');
 const extractMessages = require('../extractMessages/extractMessages');
 const generatePOT = require('../generatePOT/generatePOT');
 
+/**
+ * Comparison function for sorting i18n message contexts
+ * @param {Object} ctx1
+ * @param {String} ctx1.file
+ * @param {Number} ctx1.line
+ * @param {Object} ctx2
+ * @param {String} ctx2.file
+ * @param {Number} ctx2.line
+ * @returns {Number}
+ */
+function compareContexts(ctx1, ctx2) {
+    if (ctx1.file > ctx2.file) {
+        return 1;
+    }
+    if (ctx1.file < ctx2.file) {
+        return -1;
+    }
+    return ctx1.line > ctx2.line ? 1 : 0;
+}
+
+/**
+ * Rollup plugin - extracts i18n messages from a source codebase, and writes POT file as output
+ * @param {Object} options
+ * @returns {Object} Rollup plugin API
+ */
 module.exports = (options = {}) => {
     if (typeof options.output !== 'string') {
         throw new Error('"output" has to be defined');
@@ -38,28 +63,20 @@ module.exports = (options = {}) => {
             ) {
                 extractMessages(source, id, path.dirname(options.output)).forEach((context, message) => {
                     const contexts = [...(messages.get(message) || []), ...context];
-                    // sort the context to have the same order everytime
+                    // sort by context file:line to have the same order everytime
                     messages.set(
                         message,
-                        contexts.sort((a, b) => {
-                            if (a.file > b.file) {
-                                return 1;
-                            }
-
-                            if (a.file < b.file) {
-                                return -1;
-                            }
-
-                            return a.line > b.line ? 1 : 0;
-                        })
+                        contexts.sort(compareContexts)
                     );
                 });
             }
             return null;
         },
         buildEnd() {
-            // sort the messages to have the same order everytime
-            const sortedMessages = new Map([...messages].sort());
+            // sort the messages by first context's file:line to have the same order everytime
+            const sortedMessages = new Map([...messages].sort((a, b) => {
+                return compareContexts(a[1][0], b[1][0]);
+            }));
             return fs.promises.writeFile(options.output, generatePOT(sortedMessages));
         }
     };
